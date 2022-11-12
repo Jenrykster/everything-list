955cc1a0c70e2af96ee836ccc62babd40d94ed72
João Henrique <joaoenrique.san@gmail.com>
2022-11-12 17:19:25 -0300

    chore: initial setup

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import * as dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
