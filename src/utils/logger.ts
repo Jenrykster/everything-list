94e79a614ac21db1bfaca5a3576ce0cdbe735028
João Henrique <joaoenrique.san@gmail.com>
2022-11-12 21:26:30 -0300

    feat: improve the logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,14 +1,29 @@
 /* eslint-disable no-console */
 
+const BAR = '==========================================';
+const INFO_COLOR_PREFIX = '\x1b[36m';
+const ERROR_COLOR_PREFIX = '\x1b[31m';
+
 const info = (...params: any[]) => {
-  console.info('\x1b[36mINFO:', ...params);
+  console.info(`${INFO_COLOR_PREFIX}INFO:`, ...params);
 };
 
 const error = (...params: any[]) => {
-  console.error('\x1b[31mERROR:', ...params);
+  if (params.length === 1 && params[0] instanceof Error) {
+    console.log(ERROR_COLOR_PREFIX);
+    console.log(BAR);
+    console.log(`ERROR: ${params[0].name}`);
+    console.log(`DESCRIPTION: ${params[0].message}`);
+    console.log(`STACK: ${params[0].stack}`);
+    console.log(BAR);
+  } else {
+    console.error(`${ERROR_COLOR_PREFIX}ERROR: `, ...params);
+  }
 };
 
-export {
+const logger = {
   info,
   error,
 };
+
+export default logger;
